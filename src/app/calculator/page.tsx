4274d04b8f4fa1c7ba1a7601103b71812628ea9e
Diego Allies <91248430+diegoallies@users.noreply.github.com>
2025-04-06 17:15:28 +0200

    Update page.tsx

--- src/app/calculator/page.tsx
+++ src/app/calculator/page.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { useSheepStore, Sheep } from '../lib/useSheepStore'
 
 export default function LivestockCalculatorPage() {
